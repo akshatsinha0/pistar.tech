c645d3d6f0721afd5d42bd0f76c2721abd4bc5d8
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-09-26 03:41:15 +0530

    changes

--- src/components/forms/book-a-demo.tsx
+++ src/components/forms/book-a-demo.tsx
@@ -1,18 +1,7 @@
 "use client";
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-// internal
-import google from '@/assets/images/icon/google.png';
-import facebook from '@/assets/images/icon/facebook.png';
-import RegisterForm from "../forms/register-form";
-import LoginForm from "../forms/login-form";
-import { Auth } from "firebase/auth";
-import AuthLogin from "../forms/auth-login";
-import AuthSignup from "../forms/auth-signup";
-import CareerVendorForm from "./career-vendor-form";
-import CareerJobForm from "./career-job-form";
 import BookDemoForm from "./book-demo-form";
+import BackArrowButton from "@/components/common/back-arrow-button";
 const YT_EMBED_URL = "https://www.youtube.com/embed/oCleslN0drs?autoplay=1&mute=1&rel=0&controls=0&modestbranding=1&iv_load_policy=3&playsinline=1&disablekb=1&fs=0&loop=1&playlist=oCleslN0drs";
 const BookADemo = () => {
   const [ showVideo, setShowVideo ] = React.useState(false);
@@ -42,15 +31,7 @@ const BookADemo = () => {
       <div className="modal-dialog modal-dialog-centered mb-10 mt-10" style={{width:"75vw",maxWidth:"75vw"}}>
         <div className="container-fluid d-flex align-items-center justify-content-center">
 <div className="user-data-form modal-content p-0" style={{boxShadow: "2px 5px 20px rgba(0, 0, 0, 0.1)",background: "#EDF1EE",height:"90vh",overflow:"hidden", position:"relative"}} >
-          <button
-              type="button"
-className="btn-back-arrow"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-style={{position:"absolute", left:6, top:6, zIndex:5}}
-            >
-              <i className="bi bi-arrow-left"></i>
-            </button>
+          <BackArrowButton data-bs-dismiss="modal" aria-label="Close" />
             <div className="row g-0 w-100 h-100">
               {/* Left: form */}
               <div className="col-12 col-lg-6 d-flex flex-column h-100 overflow-auto">
